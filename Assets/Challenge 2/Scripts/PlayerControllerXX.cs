946c828687fa7b0b632d3bb7634854ffabefe7e2
Mike <netterminalgenesd@gmail.com>
2020-02-28 14:41:54 +0100

    challenge 2 solved

---
+++ Assets/Challenge 2/Scripts/PlayerControllerXX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerXX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogRate = 0.5f;
+    private float nextDog = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextDog)
+        {
+            nextDog = Time.time + dogRate;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            
+        }
+    }
+}
